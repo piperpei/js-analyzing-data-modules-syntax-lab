ab777e34d226c71bb81ffbcdc66d4d510cef6633
piperpei <piperpei@yahoo.com>
2025-08-22 15:12:48 -0700

    Array merging to object and todays date

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+// Import datejs
+require('datejs');
 
+// Combine user array to one object 
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
 
+  // Add users from arrays
+  for (const arr of args) {
+    combinedObject.users.push(...arr);
+  }
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  // Add Today's Date 
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+  
+  return combinedObject;
+}
+
+module.exports = { 
+  ...(typeof combineUsers !== 'undefined' && { combineUsers }) 
 };
